8590cfc33da9de782dfc35dc10dd46d65da8442a
Joshua Hembree <jhembree@quickbase.com>
2020-06-05 11:59:41 -0700

    more store

--- store.js
+++ store.js
@@ -1,3 +1,15 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import thunk from 'redux-thunk';
 
+const reducers = combineReducers();
+const middleware = [thunk]
+const composeWithDevToolsEnhancer = composeWithDevTools({
+  name: "Redux Dev Tools Logger",
+  trace: true,
+})(applyMiddleware(...middleware))
+
+export const store = createStore(reducers, composeWithDevToolsEnhancer);
+
+export const persister = persistStore(store);
